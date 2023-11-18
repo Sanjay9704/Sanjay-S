dff6371c8c2609b263359e0de3ceb0fbf49030a4
Sanjay9704 <srrisanjay9704@gmail.com>
2023-11-18 14:48:07 +0530

    save

---
+++ classwork/src/App.js
@@ -0,0 +1,10 @@
+import BasicForm from './day2/BasicForm'
+function App() {
+  return (
+    <div>
+      <BasicForm/>
+    </div>
+  );
+}
+
+export default App;
--- src/day3/Button.jsx
+++
@@ -1,8 +0,0 @@
-const Button=(props)=>
-{
-    return (<button className={`px-4 py-2 bg-blue-500 rounded-lg`}>{props.label}
-    {props.children}
-    </button>
-    );
-};
-export default Button;
